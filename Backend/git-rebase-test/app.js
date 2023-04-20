e55269dbdc4dd8e32fe4ac3ffebc2eb470398f51
songsong95 <blinglbling137@gmail.com>
2023-04-20 15:32:28 +0900

    signUp add

--- Backend/git-rebase-test/app.js
+++ Backend/git-rebase-test/app.js
@@ -1,29 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const { DataSource } = require('typeorm')
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { DataSource } = require("typeorm");
 
 const app = express();
 const appDataSource = new DataSource({
-  type: process.env.DB_TYPE,
+  type: process.env.DB_CONNECTION,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-})
+  database: process.env.DB_DATABASE,
+});
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
-app.get('/ping', (req, res) => {
-  res.json({ messgae: 'pong' });
-})
+app.get("/ping", (req, res) => {
+  res.json({ messgae: "pong" });
+});
 
 /*
 [TEST]
@@ -32,12 +32,31 @@ feature/signin 브랜치의 경우 app.post('/users/signin', ...)
 feature/signup 브랜치의 경우 app.post('/users/signup', ...)
 */
 
+app.post("/users/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  return await appDataSource.query(
+    `
+      INSERT INTO
+        users (
+          username,
+          email,
+          password			
+        )
+      VALUES (
+        ?,
+        ?,
+        ?,
+        ?
+      )
+    `,
+    [username, email, password]
+  );
+});
 
 app.listen(PORT, () => {
-  appDataSource.initialize()
-    .then(() => {
-      console.log("DB Connection has been initialized")
-    })
+  appDataSource.initialize().then(() => {
+    console.log("DB Connection has been initialized");
+  });
 
   console.log(`Listening to request on localhost:${PORT}`);
-})
+});
